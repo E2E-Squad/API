c210ffbe33c3cafeb6460b95614f37218fd36b1d
loichu <loic@humbert-web.dev>
2022-05-02 11:59:32 +0200

    Use sentinel model on delete

--- E2E/api/models/chat.py
+++ E2E/api/models/chat.py
@@ -1,18 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+def get_sentinel_user():
+    return get_user_model().objects.get_or_create(username='deleted')[0]
 
 
 class Chat(models.Model):
     confirmed = models.BooleanField(default=False)
     requested_by = models.ForeignKey(
         'Student',
-        null=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.SET(get_sentinel_user),
         related_name="requests",
     )
     reported_by = models.ForeignKey(
         'Student',
-        null=True,
-        on_delete=models.SET_NULL,
+        on_delete=models.SET(get_sentinel_user),
         related_name="reported_chats",
     )
 
@@ -22,9 +25,9 @@ class Message(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     sender = models.ForeignKey(
         'Student',
-        on_delete=models.SET_NULL
+        on_delete=models.SET(get_sentinel_user),
     )
     chat = models.ForeignKey(
         Chat,
-        on_delete=models.CASCADE
+        on_delete=models.SET(get_sentinel_user),
     )
