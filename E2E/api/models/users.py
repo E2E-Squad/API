c288c638b9e9d633c227f3dc499fcca08b7ec64f
loichu <loic@humbert-web.dev>
2022-05-03 23:45:42 +0200

    Create user working as expected

--- E2E/api/models/users.py
+++ E2E/api/models/users.py
@@ -1,10 +1,32 @@
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from ..managers import CustomUserManager
+
 
 class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+
+    email = models.EmailField(_("email address"), unique=True, null=True)
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=True,
+        null=True,
+        help_text=_(
+            "Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only."
+        ),
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
     is_student = models.BooleanField('student status', default=False)
     is_moderator = models.BooleanField('moderator status', default=False)
+
+    objects = CustomUserManager()
 
 
 class Student(models.Model):
